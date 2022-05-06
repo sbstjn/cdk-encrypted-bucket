eb4cff7fc8ec5e9359951c41a6384f7c5057e307
sbstjn <mail@sbstjn.com>
2022-05-06 14:14:25 +0200

    initial commit

---
+++ .projenrc.js
@@ -0,0 +1,16 @@
+const { awscdk } = require('projen');
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'sbstjn',
+  authorAddress: 'mail@sbstjn.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'encrypted-bucket',
+  repositoryUrl: 'https://github.com/sbstjn/cdk-encrypted-bucket.git',
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The 'name' in package.json. */
+});
+
+project.synth();
