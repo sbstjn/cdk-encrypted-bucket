d5c25f2f63f9c6e2b3db1c9b82f22e3771b19cb2
sbstjn <mail@sbstjn.com>
2022-05-06 14:19:22 +0200

    chore: update projen

--- .projenrc.js
+++ .projenrc.js
@@ -2,11 +2,16 @@ const { awscdk } = require('projen');
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'sbstjn',
+  authorName: 'Sebastian Müller',
   authorAddress: 'mail@sbstjn.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'encrypted-bucket',
   repositoryUrl: 'https://github.com/sbstjn/cdk-encrypted-bucket.git',
+  description: 'A CDK construct for creating encrypted S3 buckets',
+  docgen: true,
+  eslint: true,
+  dependabot: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
