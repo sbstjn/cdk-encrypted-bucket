571b04773fab4deacbc3f7b3188d7996a4c298cc
sbstjn <mail@sbstjn.com>
2022-05-06 16:03:02 +0200

    chore: extend tests for props

--- test/EncryptedBucket.test.ts
+++ test/EncryptedBucket.test.ts
@@ -3,18 +3,46 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { EncryptedBucket } from '../src';
 
 describe('EncryptedBucket', () => {
-  const app = new App();
-  const stack = new Stack(app, 'ExampleStack');
+  describe('Default', () => {
+    const app = new App();
+    const stack = new Stack(app, 'ExampleStack');
 
-  new EncryptedBucket(stack, 'EncryptedBucket');
+    new EncryptedBucket(stack, 'EncryptedBucket');
 
-  const template = Template.fromStack(stack);
+    const template = Template.fromStack(stack);
 
-  it('creates bucket', () => {
-    template.resourceCountIs('AWS::S3::Bucket', 1);
+    it('creates key', () => {
+      template.resourceCountIs('AWS::KMS::Key', 1);
+    });
+
+    it('creates bucket', () => {
+      template.resourceCountIs('AWS::S3::Bucket', 1);
+    });
+
+    it('creates bucket without versioning', () => {
+      const bucket = template.findResources('AWS::S3::Bucket');
+      const bucketTemplate = JSON.stringify(bucket);
+
+      expect(bucketTemplate).not.toContain('VersioningConfiguration');
+    });
   });
 
-  it('creates key', () => {
-    template.resourceCountIs('AWS::KMS::Key', 1);
+  describe('Versioning', () => {
+    const app = new App();
+    const stack = new Stack(app, 'ExampleStack');
+
+    new EncryptedBucket(stack, 'EncryptedBucket', {
+      versioned: true,
+    });
+
+    const template = Template.fromStack(stack);
+
+    it('creates bucket with versioning', () => {
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        VersioningConfiguration: {
+          Status: 'Enabled',
+        },
+      });
+    });
   });
 });
